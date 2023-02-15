790e45e43bb93a23cb91d5ad586a7872249ff50b
jerzy-vletter <vletter123@gmail.com>
2023-02-15 16:06:12 +0100

    input field + send button styled

--- darkchat.js
+++ darkchat.js
@@ -1,4 +1,7 @@
 function hideButton(){
     document.getElementById("LauchButton").classList.toggle("LauchButton-fade-out");
+    document.getElementById("texting").classList.toggle("texting-fade-in");
+    document.getElementById("sendMsg").classList.toggle("sendMsg-fade-in");
     setTimeout(() => { document.getElementById("LauchContainer").style.display = "none"; }, 500);
+    setTimeout(() => { document.getElementById("textingContainer").style.display = "block"; }, 500);
 }
